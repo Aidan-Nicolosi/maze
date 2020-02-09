76361e238da648c74918b6945b8e058acdc945ae
Aidan Nicolosi <aidan.nicolosi@gmail.com>
2020-02-09 15:09:22 -0500

    Add animation to player movement

--- game.js
+++ game.js
@@ -2,46 +2,58 @@
 initialize();
 
 function initialize() {
-    $('.up').on('click', moveUp);
-    $('.left').on('click', moveLeft);
-    $('.right').on('click', moveRight);
-    $('.down').on('click', moveDown);
+    enableMovement();
     $('.reset').on('click', reset);
 }
 
 function reset() {
-    $('.up').on('click', moveUp);
-    $('.left').on('click', moveLeft);
-    $('.right').on('click', moveRight);
-    $('.down').on('click', moveDown);
+    enableMovement();
     $('.reset').hide();
     const player = $('.player').detach();
     $('.start').append(player);
 }
 
-function disable() {
+function finish() {
+    disableMovement();
+    $('.reset').show();
+}
+
+function disableMovement() {
     $('.up').off('click');
     $('.left').off('click');
     $('.right').off('click');
     $('.down').off('click');
-    $('.reset').show();
+}
+
+function enableMovement() {
+    $('.up').on('click', moveUp);
+    $('.left').on('click', moveLeft);
+    $('.right').on('click', moveRight);
+    $('.down').on('click', moveDown);
 }
 
 function moveUp() {
     const isAllowed = $('.player').parent().hasClass('open-up');
     if (isAllowed) {
-        const originRow = $('.player').parent().attr('row');
-        const originCol = $('.player').parent().attr('col');
-        const player = $('.player').detach();
-        const targetRow = parseInt(originRow) - 1;
-        const targetCol = originCol;
-        targetSelector = '.r' + targetRow + '.c' + targetCol;
-        console.log(targetSelector);
-        $(targetSelector).append(player);
-        const isFinished = $(targetSelector).hasClass('finish');
-        if (isFinished) {
-            disable();
-        }
+        disableMovement();
+        $('.player').addClass('moving-up');
+
+        setTimeout(function () {
+            const originRow = $('.player').parent().attr('row');
+            const originCol = $('.player').parent().attr('col');
+            const player = $('.player').removeClass('moving-up').detach();
+            const targetRow = parseInt(originRow) - 1;
+            const targetCol = originCol;
+            targetSelector = '.r' + targetRow + '.c' + targetCol;
+            console.log(targetSelector);
+            $(targetSelector).append(player);
+            const isFinished = $(targetSelector).hasClass('finish');
+            if (isFinished) {
+                finish();
+            } else {
+                enableMovement();
+            }
+        }, 150)
     }
 
 }
@@ -49,54 +61,73 @@ function moveUp() {
 function moveLeft() {
     const isAllowed = $('.player').parent().hasClass('open-left');
     if (isAllowed) {
-        const originRow = $('.player').parent().attr('row');
-        const originCol = $('.player').parent().attr('col');
-        const player = $('.player').detach();
-        const targetCol = parseInt(originCol) - 1;
-        const targetRow = originRow;
-        targetSelector = '.r' + targetRow + '.c' + targetCol;
-        console.log(targetSelector);
-        $(targetSelector).append(player);
-        const isFinished = $(targetSelector).hasClass('finish');
-        if (isFinished) {
-            disable();
-        }
+        disableMovement();
+        $('.player').addClass('moving-left');
+
+        setTimeout(function () {
+            const originRow = $('.player').parent().attr('row');
+            const originCol = $('.player').parent().attr('col');
+            const player = $('.player').removeClass('moving-left').detach();
+            const targetCol = parseInt(originCol) - 1;
+            const targetRow = originRow;
+            targetSelector = '.r' + targetRow + '.c' + targetCol;
+            $(targetSelector).append(player);
+            const isFinished = $(targetSelector).hasClass('finish');
+            if (isFinished) {
+                finish();
+            } else {
+                enableMovement();
+            }
+        }, 150);
     }
 }
 
 function moveRight() {
     const isAllowed = $('.player').parent().hasClass('open-right');
     if (isAllowed) {
-        const originRow = $('.player').parent().attr('row');
-        const originCol = $('.player').parent().attr('col');
-        const player = $('.player').detach();
-        const targetCol = parseInt(originCol) + 1;
-        const targetRow = originRow;
-        targetSelector = '.r' + targetRow + '.c' + targetCol;
-        console.log(targetSelector);
-        $(targetSelector).append(player);
-        const isFinished = $(targetSelector).hasClass('finish');
-        if (isFinished) {
-            disable();
-        }
+        disableMovement();
+        $('.player').addClass('moving-right');
+
+        setTimeout(function () {
+            const originRow = $('.player').parent().attr('row');
+            const originCol = $('.player').parent().attr('col');
+            const player = $('.player').removeClass('moving-right').detach();
+            const targetCol = parseInt(originCol) + 1;
+            const targetRow = originRow;
+            targetSelector = '.r' + targetRow + '.c' + targetCol;
+            $(targetSelector).append(player);
+            const isFinished = $(targetSelector).hasClass('finish');
+            if (isFinished) {
+                finish();
+            } else {
+                enableMovement();
+            }
+        }, 150);
     }
 }
+
 
 function moveDown() {
     const isAllowed = $('.player').parent().hasClass('open-down');
     if (isAllowed) {
-        const originRow = $('.player').parent().attr('row');
-        const originCol = $('.player').parent().attr('col');
-        const player = $('.player').detach();
-        const targetRow = parseInt(originRow) + 1;
-        const targetCol = originCol;
-        targetSelector = '.r' + targetRow + '.c' + targetCol;
-        console.log(targetSelector);
-        $(targetSelector).append(player);
-        const isFinished = $(targetSelector).hasClass('finish');
-        if (isFinished) {
-            disable();
-        }
+        disableMovement();
+        $('.player').addClass('moving-down');
+
+        setTimeout(function () {
+            const originRow = $('.player').parent().attr('row');
+            const originCol = $('.player').parent().attr('col');
+            const player = $('.player').removeClass('moving-down').detach();
+            const targetRow = parseInt(originRow) + 1;
+            const targetCol = originCol;
+            targetSelector = '.r' + targetRow + '.c' + targetCol;
+            $(targetSelector).append(player);
+            const isFinished = $(targetSelector).hasClass('finish');
+            if (isFinished) {
+                finish();
+            } else {
+                enableMovement();
+            }
+        }, 150);
+
     }
 }
-
